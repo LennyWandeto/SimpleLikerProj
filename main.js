e0da24474b7ed75c1f584583d3ced067ded136b3
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-04-06 10:44:47 +0300

    final completion

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likebtns = document.querySelectorAll('.like-glyph')
+const modal = document.getElementById('modal')
+const modalMessage = document.getElementById('modal-message')
+likebtns.forEach(span => span.addEventListener ('click', handleHeartClick));
 
+function handleHeartClick(e) {
+// console.log(e.target);
+mimicServerCall()
+.then(() => handleSuccess(e.target))
+.catch(handleError)
+}
+function handleSuccess(liked){
+  if(liked.textContent === EMPTY_HEART) {
+ liked.textContent = FULL_HEART;
+ liked.classList.add('activated-heart')
+  } else {
+    liked.textContent = EMPTY_HEART;
+    liked.classList.remove('activated-heart')
+  }
+}
+function handleError(message){
+  modal.classList.remove('hidden')
+  modalMessage.textContent = message;
+  setTimeout(() => modal.classList.add('hidden'), 3000)
 
-
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
